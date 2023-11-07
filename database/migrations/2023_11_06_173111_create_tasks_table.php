9f19a011c5917225865ff69ad7f3bcee313c3426
Nazar <nazarskrinskyi@gmail.com>
2023-11-07 07:27:10 +0200

    Created tasks table

--- database/migrations/2023_11_06_173111_create_tasks_table.php
+++ database/migrations/2023_11_06_173111_create_tasks_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('parent_id')->nullable()->constrained('tasks');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->enum('status', ['todo', 'done']);
+            $table->unsignedTinyInteger('priority');
+            $table->string('title',255);
+            $table->text('description')->nullable();
             $table->timestamps();
+            $table->timestamp('completed_at')->nullable();
+            $table->index('status');
+            $table->index('priority');
+            $table->index('title');
         });
     }
 
