4f0f1de3cef99e8997d94e0128edc6caf984d0c6
Nazar <nazarskrinskyi@gmail.com>
2023-11-07 22:59:07 +0200

    Create Requests for creating and updating

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'status' => $this->status,
+            'priority' => $this->priority,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'completed_at' => $this->completed_at,
+            'parent_task' => new TaskResource($this->parentTask),
+            'subtasks' => $this->subtasks,
+        ];
+    }
+}
